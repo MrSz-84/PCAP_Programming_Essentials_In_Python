3ba12d2ed623e04c737c99d2d2b5f4196acd2f89
MrSz-84 <126359057+MrSz-84@users.noreply.github.com>
2023-04-18 22:53:55 +0200

    :rocket: Sudoku condition check added 2 out of 3. :rocekt: Rows and columns as functions. What remains: 3x3 squares checkup :boom:

--- Sudoku.py
+++ Sudoku.py
@@ -2,14 +2,32 @@
 # Reads if given numbers are sudoku or not
 from sys import stdin
 
-# quick check if git control and staging works from ubuntu
-# another check from vsc from ubuntu machine
 
+def checkup(checkup_lst):  # check if each sudoku row is valid
+    for row in checkup_lst:
+        is_sudoku_rows = row
+        for i in digits_pattern:
+            if i not in is_sudoku_rows:
+                return False
+    return True
+
+
+def column_rearrange(sudoku):  # nested list for each column of sudoku as feed for checkup
+    for row in range(9):
+        temporary_list = []
+        for column in range(9):
+            temporary_list.append(sudoku[column][row])
+        checkup_lst.append(temporary_list)
+    return checkup(checkup_lst)
+
+
+# Prepare data input from user. Clean out end of line signs and return data to a list.
 # print('Check if given numbers are Sudoku or not.\n'
 #       'Data has to be 9 rows, 9 digits per row\n'
 #       'NO SPACES ARE ALLOWED!')
 # bucket = stdin.readlines(9*9)
-bucket = ['295743861\n', '431865927\n', '876192543\n', '387459216\n', '612387495\n', '549216738\n', '763524189\n', '928671354\n', '154938672\n']
+bucket = ['295743861\n', '431865927\n', '876192543\n', '387459216\n', '612387495\n',
+          '549216738\n', '763524189\n', '928671354\n', '154938672\n']
 before_sudoku = []
 sstring = ''
 for index in bucket:
@@ -19,6 +37,20 @@ for index in bucket:
         sstring += digit
     before_sudoku.append(sstring)
     sstring = ''
-for i in before_sudoku:
-    print(i, len(i))
 
+# Creating sudoku grid for checkup if requirements are met.
+
+sudoku = [[int(row) for row in column] for column in before_sudoku]
+del before_sudoku, sstring
+digits_pattern = [i + 1 for i in range(len(sudoku))]
+
+# Boolean checkups
+
+checkup_lst = sudoku[:]
+rows_ok = checkup(checkup_lst)
+print(rows_ok)
+checkup_lst.clear()
+cols_ok = column_rearrange(sudoku)
+print(cols_ok)
+
+# Try to think how to make check of 3x3 list made from 3x3 slices of sudoku list.
