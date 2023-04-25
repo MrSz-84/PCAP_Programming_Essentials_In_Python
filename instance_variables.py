c45ff4fac175b843805e7087bcf61417b78b0e88
MrSz-84 <126359057+MrSz-84@users.noreply.github.com>
2023-04-25 22:40:15 +0200

    Object programming module. Instance variables
    Signed-off-by: MrSz-84 <126359057+MrSz-84@users.noreply.github.com>

--- instance_variables.py
+++ instance_variables.py
@@ -1,11 +1,34 @@
-# %% exemplary class
+# # %% exemplary class
+#
+# class ExemplaryClass:
+#     def __init__(self, value=1):
+#         self.first = value
+#
+#     def set_second(self, value):
+#         self.second = value
+#
+#
+# exemplary_object1 = ExemplaryClass()
+# exemplary_object2 = ExemplaryClass(2)
+#
+# exemplary_object2.set_second(3)
+#
+# exemplary_object3 = ExemplaryClass(4)
+# exemplary_object3.third = 5
+#
+# print(exemplary_object1.__dict__)
+# print(exemplary_object2.__dict__)
+# print(exemplary_object3.__dict__)
+
+
+# %% Making properties private
 
 class ExemplaryClass:
     def __init__(self, value=1):
-        self.first = value
+        self.__first = value
 
     def set_second(self, value):
-        self.second = value
+        self.__second = value
 
 
 exemplary_object1 = ExemplaryClass()
@@ -14,8 +37,10 @@ exemplary_object2 = ExemplaryClass(2)
 exemplary_object2.set_second(3)
 
 exemplary_object3 = ExemplaryClass(4)
-exemplary_object3.third = 5
+exemplary_object3.__third = 5
 
 print(exemplary_object1.__dict__)
 print(exemplary_object2.__dict__)
 print(exemplary_object3.__dict__)
+
+print(exemplary_object1._ExemplaryClass__first)
