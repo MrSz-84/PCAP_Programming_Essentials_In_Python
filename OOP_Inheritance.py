fee73389a6105cd18bcfaebf64e5af80478c8470
MrSz-84 <126359057+MrSz-84@users.noreply.github.com>
2023-05-02 21:22:50 +0200

    :poop: Finished chapter about inheritance. It's quite cool actually!:tada:

--- OOP_Inheritance.py
+++ OOP_Inheritance.py
@@ -167,40 +167,367 @@
 
 # %% 6.5.1.11 OOP -> Inheritance - how Python language finds
 # properties and methods: continuation.
-class Lvl1:
-    variable_1 = 100
+# class Lvl1:
+#     variable_1 = 100
 
-    def __init__(self) -> None:
-        self.var_1 = 101
+#     def __init__(self) -> None:
+#         self.var_1 = 101
 
-    def fun_1(self):
-        return 102
-
-
-class Lvl2(Lvl1):
-    variable_2 = 200
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.var_2 = 201
-
-    def fun_2(self):
-        return 202
+#     def fun_1(self):
+#         return 102
 
 
-class Lvl3(Lvl2):
-    variable_3 = 300
+# class Lvl2(Lvl1):
+#     variable_2 = 200
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.var_3 = 301
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.var_2 = 201
 
-    def fun_3(self):
-        return 302
+#     def fun_2(self):
+#         return 202
 
 
-obj = Lvl3()
+# class Lvl3(Lvl2):
+#     variable_3 = 300
 
-print(obj.variable_1, obj.var_1, obj.fun_1())
-print(obj.variable_2, obj.var_2, obj.fun_2())
-print(obj.variable_3, obj.var_3, obj.fun_3())
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.var_3 = 301
+
+#     def fun_3(self):
+#         return 302
+
+
+# obj = Lvl3()
+
+# print(obj.variable_1, obj.var_1, obj.fun_1())
+# print(obj.variable_2, obj.var_2, obj.fun_2())
+# print(obj.variable_3, obj.var_3, obj.fun_3())
+
+# %% 6.5.1.12 OOP -> Inheritance - how Python language finds
+# # properties and methods: continuation - multiple super classes
+# class SuperA:
+#     var_A = 10
+
+#     def fun_A(self):
+#         return 11
+
+
+# class SuperB:
+#     var_B = 20
+
+#     def fun_B(self):
+#         return 21
+
+
+# class Sub(SuperA, SuperB):
+#     pass
+
+
+# obj = Sub()
+
+# print(obj.var_A, obj.fun_A())
+# print(obj.var_B, obj.fun_B())
+
+# %% 6.5.1.13 OOP -> Inheritance - how Python language finds
+# properties and methods: continuation - inheritance the same function name
+# or property name.
+# class Lvl1:
+#     var = 100
+
+#     def fun(self):
+#         return 101
+
+
+# class Lvl2:
+#     var = 200
+
+#     def fun(self):
+#         return 201
+
+
+# class Lvl3(Lvl2):
+#     pass
+
+
+# obj = Lvl3()
+
+# print(obj.var, obj.fun())
+
+# %% 6.5.1.14 OOP -> Inheritance - how Python language finds
+# properties and methods: continuation - inheritance the same function name
+# or property name from multiple super classes.
+# class Left:
+#     var = 'L'
+#     var_left = 'LL'
+
+#     def fun(self):
+#         return 'Left'
+
+
+# class Right:
+#     var = 'R'
+#     var_right = 'RR'
+
+#     def fun(self):
+#         return 'Right'
+
+
+# class Sub(Right, Left):
+#     pass
+
+
+# obj = Sub()
+
+# print(obj.var, obj.var_left, obj.var_right, obj.fun())
+
+# %% 6.5.1.15 OOP -> Inheritance - how to create class hierarchy
+# class One:
+#     def do_it(self):
+#         print('do_it from One')
+
+#     def doanything(self):
+#         self.do_it()
+
+
+# class Two(One):
+#     def do_it(self):
+#         print('do_it from Two')
+
+
+# one = One()
+# two = Two()
+
+# one.doanything()
+# two.doanything()
+
+# %% 6.5.1.16 OOP -> Inheritance - how to create class hierarchy: continuation
+# import time
+
+
+# class TrackedVehicles:
+#     def tracks_control(left, stop):
+#         pass
+
+#     def turn(left):
+#         tracks_control(left, True)
+#         time.sleep(0.25)
+#         tracks_control(left, False)
+
+
+# class WheelVehicles:
+#     def turn_front_wheels(left, on):
+#         pass
+
+#     def turn(left):
+#         turn_front_wheels(left, True)
+#         time.sleep(0.25)
+#         turn_front_wheels(left, False)
+
+# %% 6.5.1.17 OOP -> Inheritance - how to create class hierarchy: continuation
+# import time
+
+
+# class Vehicles:
+
+#     def change_direction(left, on):
+#         pass
+
+#     def turn(left):
+#         change_direction(left, True)
+#         time.sleep(0.25)
+#         change_direction(left, False)
+
+
+# class TrackedVehicles(Vehicles):
+#     def tracks_control(left, stop):
+#         pass
+
+#     def change_direction(left, on):
+#         tracks_control(left, on)
+
+
+# class WheeledVehicles(Vehicles):
+#     def turn_front_wheels(left, on):
+#         pass
+
+#     def change_direction(left, on):
+#         turn_front_wheels(left, on)
+
+# %% 6.5.1.18 OOP -> Inheritance - how to create class hierarchy: continuation
+# import time
+
+
+# class TrackedVehicles():
+#     def change_direction(self, left, on):
+#         print('tracks: ', left, on)
+
+
+# class WheeledVehicles():
+#     def change_direction(self, left, on):
+#         print('wheels: ', left, on)
+
+
+# class Vehicles:
+#     def __init__(self, controler) -> None:
+#         self.controler = controler
+
+#     def turn(self, left):
+#         self.controler.change_direction(left, True)
+#         time.sleep(0.25)
+#         self.controler.change_direction(left, False)
+
+
+# wheeled = Vehicles(WheeledVehicles())
+# tracked = Vehicles(TrackedVehicles())
+
+# wheeled.turn(True)
+# tracked.turn(False)
+
+# %% 6.5.1.20 OOP -> Inheritance - key issues
+class Mouse:
+    def __init__(self, name):
+        self.my_name = name
+
+    def __str__(self):
+        return self.my_name
+
+
+the_mouse = Mouse('mickey')
+print(the_mouse)  # Daje na wyjściu "mickey".
+
+
+# %%
+class Mouse:
+    pass
+
+
+class LabMouse(Mouse):
+    pass
+
+
+print(issubclass(Mouse, LabMouse), issubclass(
+    LabMouse, Mouse))  # Daje na wyjściu "False True"
+
+
+# %%
+class Mouse:
+    pass
+
+
+class LabMouse(Mouse):
+    pass
+
+
+mickey = Mouse()
+# Daje na wyjściu "True False".
+print(isinstance(mickey, Mouse), isinstance(mickey, LabMouse))
+
+
+# %%
+class Mouse:
+    pass
+
+
+mickey = Mouse()
+minnie = Mouse()
+cloned_mickey = mickey
+# Daje na wyjściu "False True".
+print(mickey is minnie, mickey is cloned_mickey)
+
+
+# %%
+class Mouse:
+    def __str__(self):
+        return "Mouse"
+
+
+class LabMouse(Mouse):
+    def __str__(self):
+        return "Laboratory " + super().__str__()
+
+
+doctor_mouse = LabMouse()
+print(doctor_mouse)  # Prints "Laboratory Mouse".
+
+
+# %%
+class Mouse:
+    Population = 0
+
+    def __init__(self, name):
+        Mouse.Population += 1
+        self.name = name
+
+    def __str__(self):
+        return "Hi, my name is " + self.name
+
+
+class LabMouse(Mouse):
+    pass
+
+
+professor_mouse = LabMouse("Professor Mouser")
+# Prints "Hi, my name is Professor Mouser 1"
+print(professor_mouse, Mouse.Population)
+
+
+# %%
+class Mouse:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name
+
+
+class AncientMouse(Mouse):
+    def __str__(self):
+        return "Meum nomen est " + self.name
+
+
+mus = AncientMouse("Caesar")  # Prints "Meum nomen est Caesar"
+print(mus)
+
+
+# %% 6.5.1.21 OOP -> Inheritance - exercises
+class Dog:
+    kennel = 0
+
+    def __init__(self, breed) -> None:
+        self.breed = breed
+        Dog.kennel += 1
+
+    def __str__(self) -> str:
+        return self.breed + ' says: Woof!'
+
+
+class SheepDog(Dog):
+    def __str__(self) -> str:
+        return super().__str__() + ' Don\'t run little sheep!'
+
+
+class LowlandDog(SheepDog):
+    def __str__(self) -> str:
+        return Dog.__str__(self) + ' I don\'t like mountains!'
+
+
+class GuardDog(Dog):
+    def __str__(self) -> str:
+        return super().__str__() + ' Stay where you are mister trespasser!'
+
+
+rocky = SheepDog('Collie')
+luna = GuardDog('Dobermann')
+erwin = LowlandDog('Valee Sheepdog')
+
+print(rocky)
+print(luna)
+print(erwin)
+
+print(issubclass(SheepDog, Dog), issubclass(SheepDog, GuardDog))
+print(isinstance(rocky, GuardDog), isinstance(luna, GuardDog))
+
+print(luna is luna, rocky is luna)
+print(rocky.kennel)
