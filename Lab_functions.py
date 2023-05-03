7a35ffed9613564e12f143e3bc3369ea4dd5d547
Marcin <pixelmotionhd@gmail.com>
2023-05-03 20:48:36 +0200

    Another attempt to this Lab :poop:

--- Lab_functions.py
+++ Lab_functions.py
@@ -33,7 +33,7 @@
 #     else:
 #         print("Negative results")
 
-# Leap normal year and month's lenght function
+# Leap normal year and month's length function
 
 # def if_leap(year):
 #     if year < 1582:
@@ -48,7 +48,7 @@
 #         else:
 #             return True # leap year
 
-# def month_lenght(year, month):
+# def month_length(year, month):
 #     a = if_leap(year)
 #     if a:
 #         if month == 2:
@@ -72,13 +72,11 @@
 # 	r = test_years[i]
 # 	m = test_months[i]
 # 	print(r, m, "-> ", end="")
-# 	result = month_lenght(r, m)
+# 	result = month_length(r, m)
 # 	if result == test_days[i]:
 # 		print("OK")
 # 	else:
 # 		print("Test negative")
-
-  
 
 
 # def czy_przestepny(rok):
@@ -114,7 +112,7 @@
 # 		print("Nie powiodło się")
 
 
-# Leap Normal year month's lenght and day of the year function
+# Leap Normal year month's length and day of the year function
 
 # def if_leap(year):
 #     if year < 1582:
@@ -129,7 +127,7 @@
 #         else:
 #             return True # leap year
 
-# def month_lenght(year, month):
+# def month_length(year, month):
 #     a = if_leap(year)
 #     if a:
 #         if month == 2:
@@ -177,52 +175,90 @@
 # print(day_of_the_year(1992, 7, 22))
 
 
-# Leap Normal year month's lenght and day of the year function
+# Leap Normal year month's length and day of the year function
 
-def if_leap(year):
-    if year < 1582:
+# def if_leap(year):
+#     if year < 1582:
+#         return None
+#     else:
+#         if year % 4 != 0: # normal year
+#             return False
+#         elif year % 100 != 0: # leap year
+#             return True
+#         elif year % 400 != 0: # normal year
+#             return False
+#         else:
+#             return True # leap year
+#
+# def month_length(year, month):
+#     a = if_leap(year)
+#     if a:
+#         if month == 2:
+#             return(29)
+#         elif month not in [4, 6, 9, 11]:
+#             return(31)
+#         else:
+#             return(30)
+#     else:
+#         if month == 2:
+#             return(28)
+#         elif month not in [4, 6, 9, 11]:
+#             return(31)
+#         else:
+#             return(30)
+#
+# def day_of_the_year(year, month, day):
+#     if month > 12 or month < 1 or year < 1582 or day < 1 or day > 31 or if_leap == None:
+#         None
+#
+#     days_counter = 0
+#
+#     for i in range(1, month):
+#         days_counter += month_length(year, i)
+#     return(days_counter + day)
+#
+#
+# print(day_of_the_year(2000, 12, 31))
+# print(day_of_the_year(1992, 8, 17))
+# print(day_of_the_year(2007, 6, 15))
+# print(day_of_the_year(2015, 2, 3))
+# print(day_of_the_year(2128, 8, 15))
+# print(day_of_the_year(1992, 7, 22))
+
+
+def is_year_leap(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def days_in_month(year, month):
+    lst = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if year < 1582 or month > 12 or month < 1:
         return None
-    else:
-        if year % 4 != 0: # normal year
-            return False
-        elif year % 100 != 0: # leap year
-            return True
-        elif year % 400 != 0: # normal year
-            return False
-        else:
-            return True # leap year
-
-def month_lenght(year, month):
-    a = if_leap(year)
-    if a:
+    if is_year_leap(year):
         if month == 2:
-            return(29)
-        elif month not in [4, 6, 9, 11]:
-            return(31)
-        else:
-            return(30)
-    else:
-        if month == 2:
-            return(28)
-        elif month not in [4, 6, 9, 11]:
-            return(31)
-        else:
-            return(30)
-            
-def day_of_the_year(year, month, day):
-    if month > 12 or month < 1 or year < 1582 or day < 1 or day > 31 or if_leap == None:
-        None
-    
-    days_counter = 0
+            return 29
+        return lst[month - 1]
+    elif not is_year_leap(year):
+        return lst[month - 1]
 
-    for i in range(1, month):
-        days_counter += month_lenght(year, i)
-    return(days_counter + day)
-    
 
-print(day_of_the_year(2000, 12, 31))
-print(day_of_the_year(1992, 8, 17))
-print(day_of_the_year(2007, 6, 15))
-print(day_of_the_year(2015, 2, 3))
-print(day_of_the_year(2128, 8, 15))
-print(day_of_the_year(1992, 7, 22))
+def day_of_year(year, month, day):
+    day_sum = 0
+    if year < 1582 or month > 12 or month < 1 or day < 1 or day > 31:
+        return None
+    for mt in range(month - 1):
+        day_sum += days_in_month(year, mt + 1)
+    return day_sum + day
+
+
+print(day_of_year(2000, 12, 31))
+print(day_of_year(2001, 12, 31))
+print(day_of_year(2002, 12, 30))
+print(day_of_year(2004, 12, 30))
+print(day_of_year(2023, 5, 3))
+print(day_of_year(2000, 12, 31))
+print(day_of_year(1992, 8, 17))
+print(day_of_year(2007, 6, 15))
+print(day_of_year(2015, 2, 3))
+print(day_of_year(2128, 8, 15))
+print(day_of_year(1992, 7, 22))
