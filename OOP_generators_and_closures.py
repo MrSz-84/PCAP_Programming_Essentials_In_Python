cd7cf0f588315f3ce4e7b0d8fbe2aff549cd6541
MrSz-84 <126359057+MrSz-84@users.noreply.github.com>
2023-06-10 21:57:19 +0200

    After switching to englis version of the course I've added some new lessons

--- OOP_generators_and_closures.py
+++ OOP_generators_and_closures.py
@@ -62,5 +62,77 @@
 #     print(i)
 
 # %% OOP 6.7.1.4 Generators and closures -> yield instruction
+# %% The yield statement
+# def fun(n):
+#     for i in range(n):
+#         yield i
+
+# %% how to bild a generator
+# def fun(n):
+#     for i in range(n):
+#         yield i
+#
+#
+# for v in fun(5):
+#     print(v)
+
+# %%
+# def powers_of_2(n):
+#     power = 1
+#     for i in range(n):
+#         yield power
+#         power *= 2
+#
+#
+# for v in powers_of_2(8):
+#     print(v)
 
 
+# %% generators within list comprehensions
+# def powers_of_2(n):
+#     power = 1
+#     for i in range(n):
+#         yield power
+#         power *= 2
+#
+#
+# t = [x for x in powers_of_2(5)]
+# print(t)
+
+# %% Transforming a series of subsequent results into a list
+# def powers_of_2(n):
+#     power = 1
+#     for i in range(n):
+#         yield power
+#         power *= 2
+#
+#
+# t = list(powers_of_2(3))
+# print(t)
+
+# %% generators and "in" operator
+# def powers_of_2(n):
+#     power = 1
+#     for i in range(n):
+#         yield power
+#         power *= 2
+#
+#
+# for i in range(20):
+#     if i in powers_of_2(4):
+#         print(i)
+
+# %% The Fibonacci number generator
+def fibonacci(n):
+    p = pp = 1
+    for i in range(n):
+        if i in [0, 1]:
+            yield 1
+        else:
+            n = p + pp
+            pp, p = p, n
+            yield n
+
+
+fibs = list(fibonacci(10))
+print(fibs)
